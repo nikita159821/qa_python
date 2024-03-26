d8519c4d47ddf6966b6c8c358c7e9f1526477943
Nikita Merkulov <tele2.rumerkulov@yandex.ru>
2024-03-26 17:31:46 +0500

    testy

--- tests.py
+++ tests.py
@@ -18,12 +18,12 @@ class TestBooksCollector:
 
     @pytest.mark.parametrize('name, genre',
                              [
-                                 ['Гордость и предубеждение и зомби 2','Фантастика'],
+                                 ['Гордость и предубеждение и зомби 2', 'Фантастика'],
                                  ['Оно', 'Ужасы'],
                                  ['Звонок', 'Ужасы']
                              ]
-    )
-    def test_set_book_genre_existing_genre(self,name, genre):
+                             )
+    def test_set_book_genre_existing_genre(self, name, genre):
         collector = BooksCollector()
 
         # добавляем книгу
@@ -48,7 +48,6 @@ class TestBooksCollector:
         # проверяем, что жанр не добавился
         assert collector.get_books_genre() == {name: ''}
 
-
     # 3 тест. Добавить жанр без книги
     # books_genre и genre
     def test_set_book_genre_no_book(self):
@@ -57,11 +56,10 @@ class TestBooksCollector:
         genre = 'Фантастика'
 
         # добавляем жанр книги
-        collector.set_book_genre(name,genre)
+        collector.set_book_genre(name, genre)
 
         # проверяем, что жанр не добавился
         assert collector.get_books_genre() == {}
-
 
     # 4 тест. Проверка вывода жанра книги
     def test_get_book_genre_correct_genre(self):
@@ -74,11 +72,10 @@ class TestBooksCollector:
         # добавляем жанр книги
         collector.set_book_genre(name, genre)
         # Получаем жанр по названию книги
-        collector.get_book_genre(name)
+        book_genre = collector.get_book_genre(name)
 
         # Проверяем, что жанр "Фантастика"
-        assert collector.books_genre.get(name) == 'Фантастика'
-
+        assert book_genre == 'Фантастика'
 
     # 5 тест. Проверка вывода книг с определённым жанром
     def test_get_books_with_specific_genre_fantasy(self):
@@ -112,7 +109,6 @@ class TestBooksCollector:
 
         assert actual_result == ['Гордость и предубеждение и зомби 2']
 
-
     # 7 тест. Проверка, что не возвращаются книги, не подходящие детям
     def test_get_books_for_children_negative_genre_shows_no_result(self):
         collector = BooksCollector()
@@ -127,10 +123,8 @@ class TestBooksCollector:
         # Получаем результат с помощью тестируемого метода
         actual_result = collector.get_books_for_children()
 
-
         # Проверяем, что ничего не выведено
         assert actual_result == []
-
 
     # 8 тест. Добавляем книгу в избранное
     def test_add_book_in_favorites_success(self):
@@ -146,7 +140,7 @@ class TestBooksCollector:
         assert collector.get_list_of_favorites_books() == [name]
 
     # 9 тест. Удаляем книгу из избранного
-    def test_delete_book_from_favorites_success(self,):
+    def test_delete_book_from_favorites_success(self, ):
         collector = BooksCollector()
         name = 'Гордость и предубеждение и зомби 2'
 
@@ -158,12 +152,10 @@ class TestBooksCollector:
         # Удаляем книгу из избранного
         collector.delete_book_from_favorites(name)
 
-
         assert collector.get_list_of_favorites_books() == []
 
-
     # 10 тест. Получаем список Избранных книг
-    def test_get_list_of_favorites_books_success(self,):
+    def test_get_list_of_favorites_books_success(self, ):
         collector = BooksCollector()
         name = 'Гордость и предубеждение и зомби 2'
 
